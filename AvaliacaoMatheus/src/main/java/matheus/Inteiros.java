07a839b763017edca0e34d220b4d350fa980d9c3
maribeir <maatheus.torres92@gmail.com>
2017-12-07 01:57:07 -0200

    Update Inteiros.java

--- AvaliacaoMatheus/src/main/java/matheus/Inteiros.java
+++ AvaliacaoMatheus/src/main/java/matheus/Inteiros.java
@@ -6,15 +6,18 @@ package main.java.matheus;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
- * Aplicação que cria um número inteiro com base em outrso 2 números informados.
+ * AplicaÃ§Ã£o que cria um nÃºmero inteiro com base em outros 2 nÃºmeros informados.
  * 
  * @author Matheus
  * @version 1.0.0
@@ -27,10 +30,10 @@ public class Inteiros extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	private JLabel valorA;
-	private JTextField textA;
+	private JFormattedTextField textA;
 	
 	private JLabel valorB;
-	private JTextField textB;
+	private JFormattedTextField textB;
 	
 	private JTextField textResultado;
 	 
@@ -42,14 +45,20 @@ public class Inteiros extends JFrame {
 		JPanel campos = new JPanel();
 		JPanel botao = new JPanel();
 		JPanel resultado = new JPanel();
+		
+		NumberFormat numberFormat = NumberFormat.getNumberInstance(Locale.getDefault());
+		numberFormat.setGroupingUsed(false);
 
 		valorA = new JLabel("Valor A:");
-		textA = new JTextField(20);
+		textA = new JFormattedTextField(numberFormat);
+		textA.setColumns(20);
 		
 		valorB = new JLabel("Valor B:");
-		textB = new JTextField(20);
+		textB = new JFormattedTextField(numberFormat);
+		textB.setColumns(20);
 		
 		textResultado = new JTextField(20);
+		textResultado.setEditable(false);
 		
 		JButton botaoGerarC = new JButton("Gerar Valor C");
 		Action action = new Action();
@@ -80,21 +89,25 @@ public class Inteiros extends JFrame {
 			int qtde = 0;
 			String texto = "";
 			
+			// verifica menor quantidade de caracteres
 			if (campoA.length > campoB.length)
 			    qtde = campoB.length;
 			else
 				qtde = campoA.length;
-				
+
+			// processa os campos A e B
 			for (int i = 0; i < qtde; i++)
 				texto += campoA[i] + campoB[i];
-			
+
+			// preenche o resto do texto de resultado com o restante da string com mais caracteres
 			if (campoA.length > campoB.length)			    
 				 for (int x = campoB.length; x < campoA.length; x++)
 					 texto += campoA[x];
 			else if (campoB.length > campoA.length)			    
-			 for (int x = campoA.length; x < campoB.length; x++)
-				 texto += campoB[x];
-			
+			     for (int x = campoA.length; x < campoB.length; x++)
+				     texto += campoB[x];
+
+			// caso resultado seja maior que 1.000.000, retorna -1
 			if (Integer.parseInt(texto) > 1000000)
 				texto = "-1";
 			
